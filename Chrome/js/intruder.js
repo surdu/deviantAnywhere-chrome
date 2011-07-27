6a92e56fcf9d36dfc32f9d0813d8fb2fd7267f99
postal2600 <postal2600@yahoo.com>
2011-07-27 17:44:49 +0300

    More work on the look of dA Chrome

--- Chrome/js/intruder.js
+++ Chrome/js/intruder.js
@@ -1,6 +1,9 @@
 function insertBody()
 {
-	$("body").append("<div id='devany_chrome_wrap'> </div>");
+	$("body").append("<div id='devany_chrome_wrap'>"+
+						 "<img src='chrome-extension://cfliabmmnojpbgaaijjhnfhbjklidhfp/img/status_icons/normal.gif' />"+
+						 "<div id='devany_chrome_msg'><span class='devany_chrome_msg_item'>3CO</span><span class='devany_chrome_msg_item'>1CO</span><span class='devany_chrome_msg_item'>1CO</span></div>"+
+					 "</div>");
 }
 
 insertBody();
